255bb5d0d3c723f36f31e35945151ee400bd8bcd
maryfevlom-collab <maryfevlom@gmail.com>
2025-08-04 23:01:19 +0000

    minor changes

--- Assignment-One.py
+++ Assignment-One.py
@@ -4,17 +4,17 @@ second_number = float(input("Enter the second number: "))
 operation = input("Enter the operation (+, -, *, /): ")
 if operation == '+':
     result = first_number + second_number  
-    print(f"The result of {first_number} + {second_number} is: {result}")
+    print(f"{first_number} + {second_number} = {result}")
 elif operation == '-':
     result = first_number - second_number
-    print(f"The result of {first_number} - {second_number} is: {result}")
+    print(f"{first_number} - {second_number} = {result}")
 elif operation == '*':
     result = first_number * second_number
-    print(f"The result of {first_number} * {second_number} is: {result}")
+    print(f"{first_number} * {second_number} = {result}")
 elif operation == '/':
     if second_number != 0:
         result = first_number / second_number
-        print(f"The result of {first_number} / {second_number} is: {result}")
+        print(f"{first_number} / {second_number} = {result}")
     else:
         result = "Error: Division by zero is not allowed."
         print(result)
